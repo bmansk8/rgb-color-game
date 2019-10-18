10ebe979bf09108a7196acdba57f79bb2827c909
bmansk8 <bmansk8@yahoo.com>
2019-10-18 10:54:40 -0400

    completed
    game is complete. All the style and logic is in place. Will refactor soon.

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
-var colors = generateRandomColors(6);
-
+var numSqaures = 6;
+var colors = generateRandomColors(numSqaures);
 var sqaures = document.querySelectorAll('.sqaure');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
 var resetButton = document.querySelector('#reset');
+var easyBtn = document.querySelector('#easyBtn');
+var hardBtn = document.querySelector('#hardBtn');
+
+
+
+easyBtn.addEventListener('click', function(){
+    easyBtn.classList.add('selected');
+    hardBtn.classList.remove('selected');
+    numSqaures = 3;
+    colors = generateRandomColors(numSqaures);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < sqaures.length; i++) {
+        if(colors[i]){
+            sqaures[i].style.backgroundColor = colors[i];
+        }else{
+            sqaures[i].style.display = 'none';
+        }
+    }
+});
+
+hardBtn.addEventListener('click', function(){
+    hardBtn.classList.add('selected');
+    easyBtn.classList.remove('selected');
+    numSqaures = 6;
+    colors = generateRandomColors(numSqaures);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < sqaures.length; i++) {
+        sqaures[i].style.backgroundColor = colors[i];
+        sqaures[i].style.display = 'block';
+    }
+});
 
 resetButton.addEventListener('click', function(){
     //genreate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSqaures);
     //pick new random color
     pickedColor = pickColor();
     //change coor display
+    this.textContent = 'New Colors'
     colorDisplay.textContent = pickedColor;
     //change colors
     for (let i = 0; i < sqaures.length; i++) {
         sqaures[i].style.backgroundColor = colors[i];
     }
-    h1.style.backgroundColor = '#232323';
+    h1.style.backgroundColor = 'steelblue';
+    messageDisplay.textContent = '';
 });
 
 colorDisplay.textContent = pickedColor;
